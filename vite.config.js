553fa902c53fdc0c55d324d34f3a080b0a0f688f
KarthikRaj07 <999karthikraj@gmail.com>
2026-02-14 22:33:19 +0530

    Fix srgreg

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  root: './',
-  plugins: [react(), tailwindcss(),],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
